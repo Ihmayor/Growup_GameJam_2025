4e62d441f16f09ee33cdf41697aa1a33afa7d8bb
Isla Maine <irene_mayor@Islas-MacBook-Pro.local>
2025-08-23 16:29:53 -0700

    Added base stats to plant resources

--- grow_up_game_jam_2025/scripts/resources/Plant.gd
+++ grow_up_game_jam_2025/scripts/resources/Plant.gd
@@ -1,7 +1,6 @@
 class_name Plant extends Resource
 
 @export var base_stat : int 
-@export var shape: Array[Vector2] 
 @export var first_image: Texture2D 
 @export_enum("Lettuce", "Eggplant", "Pepper", "Potato", "Tomato", "Cucumber") var compatible_matchup: String
 @export_enum("Lettuce", "Eggplant", "Pepper", "Potato", "Tomato", "Cucumber") var incompatible_matchup: Array[String]
