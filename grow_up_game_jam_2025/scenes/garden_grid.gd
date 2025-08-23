79296aecd028acee590c8b5a88229b6c1af34615
Isla Maine <irene_mayor@Islas-MacBook-Pro.local>
2025-08-23 14:48:15 -0700

    Comitting changes

--- grow_up_game_jam_2025/scenes/garden_grid.gd
+++ grow_up_game_jam_2025/scenes/garden_grid.gd
@@ -1,7 +1,20 @@
-extends Control
+class_name GardenUI extends Control
+
+@onready var slot_scene = preload("res://slot.tscn");
+@export var main_theme : Theme
 
 func _ready() -> void:
-	var slot_scene = preload("res://slot.tscn");
-
+	generate_grid()
+	
+func generate_grid() -> void: 
+	for i in 5:
+		var row = HBoxContainer.new()
+		row.theme = main_theme
+		%GridContainer.add_child(row)
+		for j in 5:
+			var slot_instance = slot_scene.instantiate()
+			#add children to the row above 
+			row.add_child(slot_instance)
+	
 func create_slot():
 	pass
--- grow_up_game_jam_2025/slot.gd
+++ grow_up_game_jam_2025/slot.gd
@@ -1,9 +1,13 @@
-extends Control
+class_name Slot extends TextureButton
+
+var is_mouse_entered:bool
+
 
 func _process(delta:float)-> void:
 	pass;
 
-
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouse:
-		pass
+		print(event)
+	if event is InputEventMouseMotion:	
+		pass 
