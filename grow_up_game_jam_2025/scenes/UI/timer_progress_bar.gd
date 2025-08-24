588faa45f8da5ab609d08821476fc7d24955c6fa
IM <irenehmayor>
2025-08-24 08:25:41 -0700

    New Timer added

---
+++ grow_up_game_jam_2025/scenes/UI/timer_progress_bar.gd
@@ -0,0 +1,26 @@
+class_name TimerProgressBar extends Control
+
+@export var increment: int = 1.5
+
+signal on_complete
+
+func _ready() -> void:
+	$TextureProgressBar.value = 0
+	start_timer()
+
+func start_timer():
+	visible = true
+	$Timer.start()
+
+func _physics_process(delta: float):
+	if $TextureProgressBar.value == $TextureProgressBar.max_value:
+		on_complete.emit()
+		stop_timer()
+
+func stop_timer():
+	$Timer.stop()
+	visible = false
+	$TextureProgressBar.value = 0
+
+func _on_timer_timeout() -> void:
+	$TextureProgressBar.value += increment
